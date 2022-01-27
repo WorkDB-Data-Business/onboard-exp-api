2493d3d63b2699aca5599c1e98256d8315e260fb
Joseph Alexandre <j.alexandremmfreitas@gmail.com>
2022-01-27 11:10:58 -0300

    ajuste cors

--- src/main/java/br/com/harvest/onboardexperience/configurations/application/SecurityConfiguration.java
+++ src/main/java/br/com/harvest/onboardexperience/configurations/application/SecurityConfiguration.java
@@ -93,6 +93,18 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	        }
 	    });
 	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+//        config.setAllowCredentials(true);
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*");
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
+	}
 	
 	@Bean
 	public RoleHierarchy roleHierarchy() {
