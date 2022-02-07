1b911d020aa31044fdf9041a01012b40fddc75fd
Jader1948 <jader.marcelino1995@gmail.com>
2022-02-07 13:06:37 -0300

    thumbnail do quiz

--- src/main/java/br/com/harvest/onboardexperience/domain/entities/Question.java
+++ src/main/java/br/com/harvest/onboardexperience/domain/entities/Question.java
@@ -28,9 +28,11 @@ public class Question {
     @Column(name = "name")
     private String name;
 
-
     @Column(name = "description")
     private String description;
+
+    @Column(name = "preview_image_path")
+    private String previewImagePath;
 
     @Column(name = "score_question")
     private Long scoreQuestion;
